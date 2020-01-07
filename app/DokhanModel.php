c3451f0178bf9135faa8b773e8de18358fb32ce3
trongtan189 <trongtan189@gmail.com>
2020-01-07 10:35:47 +0700

    tân

--- app/DokhanModel.php
+++ app/DokhanModel.php
@@ -7,7 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class DokhanModel extends Model
 {
     protected $table ='tbl_dokhan';
-    public $timestamps = false;
+    public $timestamps=false;
+
+
     public function tbl_congvanden()
     {
         return $this->hasMany('App\tbl_congvanden','id','id');
